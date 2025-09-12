96546d63d3bc6e59a31231b7a8eddc1e03cd7536
raven-in-westfall <66188189+raven-in-westfall@users.noreply.github.com>
2025-09-12 16:51:49 -0400

    Update PallyAddon.lua
    Adding PallyAddonIsPaladin function.

--- PallyAddon.lua
+++ PallyAddon.lua
@@ -4,3 +4,14 @@ function PallyAddonLog(message, verbose_arg)
         DEFAULT_CHAT_FRAME:AddMessage(message , 1, .4, 1);
     end
 end
+
+PALLY_ADDON_IS_PALADIN = nil
+function PallyAddonIsPaladin()
+    if PALLY_ADDON_IS_PALADIN == nil then
+        PALLY_ADDON_IS_PALADIN = false
+        if (UnitClass("player") == "Paladin") then
+            PALLY_ADDON_IS_PALADIN = true
+        end
+    end
+    return PALLY_ADDON_IS_PALADIN
+end
